8ad87b453e2ed2ad14696f69e3a2ac962b6da824
Cristhian C <ceballoscristhian4@gmail.com>
2023-02-19 17:01:53 -0500

    Taller terminado

--- src/main/java/org/example/CollectionUtils.java
+++ src/main/java/org/example/CollectionUtils.java
@@ -1,2 +1,46 @@
-package org.example;public class CollectionUtils {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class CollectionUtils {
+
+    public static <T> List<T> fillArray (Supplier<T> list, Integer count){
+        List<T>listData = new ArrayList<T>();
+        for(int i=0; i<count; i++){
+            listData.add(list.get());
+        }
+        return listData;
+    }
+
+    public static <T,R> List<R> TransformList (List<T> list, Function<T,R> transform) {
+        List<R> listData = new ArrayList<>();
+        for(T value: list){
+            listData.add(transform.apply(value));
+        }
+        return listData;
+    }
+    public static <T>void printList (List<T> list) {
+        for(T value: list){
+            System.out.println("Valor: "+value);
+        }
+    }
+
+    public static <T> List<T> filter (List<T> list,Predicate<T> filterNumbers){
+        List<T> listData = new ArrayList<>();
+        for (T value: list){
+            if(filterNumbers.test(value)){
+                listData.add(value);
+            }
+        }
+        return listData;
+    }
+    public static <T> T reduceList (List<T> list, BinaryOperator<T> add){
+        return list.stream().reduce(add).get();
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Punto 1
+        List<Integer> firstList = new ArrayList<Integer>(CollectionUtils.fillArray(()->new Random().nextInt(10),10));
+        List<String>  firstListString=new ArrayList<>(CollectionUtils.fillArray(()->new Random().toString(),10));
+        //Punto 2
+        List<Integer> secondList = new ArrayList<Integer>(CollectionUtils.TransformList(firstList,value->value*value));
+        //Punto 3
+        CollectionUtils.printList(firstList);
+        //Punto 4
+        List<Integer> thirdListString=new ArrayList<>(CollectionUtils.filter(secondList,value->value>10));
+        //Punto 5
+        Integer total = CollectionUtils.reduceList(firstList,(number1,number2)->number1+number2);
     }
 }
